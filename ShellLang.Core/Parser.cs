79a833d572288bd4e05748ecc6dc79c70231c2e1
Віталій Пятницький <Vitalii.Piatnytskyi.mPZIP.2022@edu.lpnu.ua>
2023-05-15 14:08:03 +0300

    Added parser initial class

---
+++ ShellLang.Core/Parser.cs
@@ -0,0 +1,21 @@
+﻿using ShellLang.Core.Enums;
+using ShellLang.Core.Models.Nodes;
+
+namespace ShellLang.Core
+{
+    public class Parser
+    {
+        private readonly IEnumerable<KeyValuePair<string, Tag>> _tokens;
+
+        public Parser(IEnumerable<KeyValuePair<string, Tag>> tokens)
+        {
+            _tokens = tokens;
+        }
+
+        public IEnumerable<INode> FormTree()
+        {
+            throw new NotImplementedException();
+            
+        }
+    }
+}
